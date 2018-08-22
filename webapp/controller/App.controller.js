d164bcadfa3aae9fc2df7ba0fa5d656a7815de36
Chen <edmund.chen@sap.com>
2018-08-22 16:23:27 +0800

    [cleanup] Code Cleanup

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -6,14 +6,5 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("demoView.controller.App",{
-		onShowHello : function () {
-			// read msg from i18n model
-			var oBundle = this.getView().getModel("i18n").getResourceBundle();
-			var sLastName = this.getView().getModel("model").getProperty("/recipient/lastName");
-			var sFirstName = this.getView().getModel("model").getProperty("/recipient/firstName");
-			var sMsg = oBundle.getText("helloMsg", [sFirstName, sLastName]);
-			// show message
-			MessageToast.show(sMsg);
-		}
 	});
 })
